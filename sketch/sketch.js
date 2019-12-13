d07876b50cf83be0953eb8d936f2c28601cf9f93
miopio <mioakasako@gmail.com>
2019-12-13 18:29:49 -0500

    sketch templates

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -1,23 +1,109 @@
-function setup() {
-  // set the width & height of the sketch
-  createCanvas(2550, 3300)
-
-  // draw will be called this many times per second
-  //frameRate(60)
-}
 
 function preload() {
     // load the CSV data into our `table` variable and clip out the header row
     both_fem = loadTable("data/bothfem_ratio.csv", "csv", "header");
     first_author = loadTable("data/firstauthor_ratio.csv", "csv", "header");
-    gender_ratio = loadTable("data/gender_ratio.csv", "csv", "header")
+    gender_ratio = loadTable("data/gender_ratio.csv", "csv", "header");
     last_author = loadTable("data/lastauthor_ratio.csv", "csv", "header");
     single_fem = loadTable("data/singlefem_ratio.csv", "csv", "header");
     total_counts = loadTable("data/total_counts.csv", "csv", "header");
 }
 
+function setup() {
+  // set the width & height of the sketch
+  createCanvas(2000, 2500);
+  //createCanvas(2550,2550);
+  background(255,255,255);
+
+  // draw will be called this many times per second
+  //frameRate(60)
+
+  //gender_ratio variables
+  total_fem = gender_ratio.getColumn('female %');
+  total_ppl = gender_ratio.getColumn('total');
+  year = gender_ratio.getColumn('year');
+
+  //total_count variables
+  paper_count = total_counts.getColumn('paper count');
+  country_count = total_counts.getColumn('country count');
+
+  //first_author variables
+  firstauth_ppl = first_author.getColumn('total');
+  firstauth_fem = first_author.getColumn('female %');
+
+  //last_author variables
+  lastauth_ppl = last_author.getColumn('total');
+  lastauth_fem = last_author.getColumn('female %');
+
+  //single_fem variables
+  single_fem_ratio = single_fem.getColumn('single fem %');
+  total_single = single_fem.getColumn('total single authors');
+
+  //both_fem variables
+  both_fem_ratio = both_fem.getColumn('both fem %');
+  total_multiple = both_fem.getColumn('total multiple authors');
+
+  //console.log(total_fem[75]);
+  console.log(single_fem.length);
+  console.log(both_fem.length);
+
+
+  total_fem_circles = []
+  console.log(year.length);
+  for(i = 73; i < 74; i++){
+    total_fem[i] = total_fem[i] * 1000;
+    colorMode(HSB);
+    strokeWeight(2);
+    noFill();
+    stroke(255, 204, 100);
+    circle(1000, 600, total_fem[i]);
+    console.log(total_fem[i]);
+    //50% and 100% circles
+    stroke('red');
+    circle(1000,600,1000);
+    circle(1000,600, 500);
+    //stroke('orange');
+    //triangleSimple(500, 1300, total_ppl[i]/300, total_ppl[i]/300);
+    stroke('orange');
+    triangleSimple(400, 2350, paper_count[i]/40, paper_count[i]/40);
+    stroke('orange');
+    triangleSimple(400, 2350, paper_count[74]/40, paper_count[74]/40);
+    //triangle(250,750,total_ppl[i], total_ppl[i], 750, 750);
+    stroke('black');
+    textSize(36);
+    text(year[i], 1500, 200);
+    console.log(year[i]);
+    //firstauth
+    stroke('green');
+    line(100, 100, 100, 100+(firstauth_ppl[i]/20));
+    //last auth
+    stroke('purple');
+    line(200, 100, 200, 100+(lastauth_ppl[i]/20));
+    //single auth
+    stroke('pink');
+    line(300, 100, 300, 100+(total_single[i]/20));
+    //multiple auth
+    stroke('lavender');
+    line(400, 100, 400, 100+(total_multiple[i]/20));
+    save("gender_"+year[i]+".jpg");
+  }
+  console.log(year[74]);
+  console.log(total_single[74]);
+  function triangleSimple(x, y, w, h){
+    // A wrapper for standard triangle() command. 
+    // triangleSimple has the lower left corner as x,y 
+    triangle(x,y, x+w/2, y-h, x+w, y);
+  }
+
+  //stroke('orange');
+  //triangleSimple(250, 750, total_ppl[i]/10, total_ppl[i]/10);
+  //save('myCanvas.jpg');
+}
+
 function draw() {
-  background(0.0)
-  noStroke()
-  circle(100, 200, 300)
+  //background(0.0)
+  //noStroke()
+  //circle(100, 200, 300);
+
+
  }
